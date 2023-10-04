267370cfb6e5b5ae4e7e41c30fb9d18ba782a03f
Alhafat <vladimir-lukin@inbox.ru>
2023-10-04 19:47:30 +0300

    04.10.2023

--- src/task_2_exсep/stringEmpty.java
+++ src/task_2_exсep/stringEmpty.java
@@ -1,10 +1,14 @@
 package task_2_exсep;
 
+import org.apache.commons.lang.StringUtils;
+
 public class stringEmpty {
 
     public static String getString(String value) throws stringNotEmpty {
 
-        if (value.isEmpty() ^ value == null) {}
-            throw new stringNotEmpty("Получена пустая строка. Произведите корректный ввод!");
+        if (value.isEmpty() || value == null || StringUtils.isBlank(value)) {
+            throw new stringNotEmpty("Получена пустая строка. Произведите корректный ввод!");}
+        return value;
     }
+
 }
